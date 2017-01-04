eccab763057a13c9f49c8fd42b7862bb0456705e
pattysc <pattys@users.noreply.github.com>
2017-01-04 13:02:36 -0500

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,46 @@
-# Implement your procedural solution here!
+# # Implement your procedural solution here!
+# def even_fibonacci_sum(n)
+#   counter = 3
+#   finalsum = 2
+#
+#   while counter < (n + 1)
+#     binding.pry
+#     if fib(counter).even?
+#       finalsum += fib(counter)
+#     end
+#
+#     counter += 1
+#   end
+#
+#   return finalsum
+# end
+#
+# def fib(n)
+#   if n == 1
+#     return 1
+#   elsif n == 2
+#     return 1
+#   else
+#     return fib(n-1) + fib(n-2)
+#   end
+# end
+
+def even_fibonacci_sum(n)
+  fibarr = [1, 2, 3]
+	finalsum = 2
+
+	while fibarr[2] < n
+
+		if fibarr[2].even?
+			finalsum += fibarr[2]
+		end
+
+		fibarr[0] = fibarr[1]
+		fibarr[1] = fibarr[2]
+
+    fibarr[2] = fibarr[0] + fibarr[1]
+
+	end
+
+	return finalsum
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,28 @@
-# Implement your object-oriented solution here!
+# # Implement your object-oriented solution here!
+class EvenFibonacci
+  attr_accessor :n
+
+  def initialize(n)
+    @n = n
+  end
+
+  def sum
+    fibarr = [1, 2, 3]
+  	finalsum = 2
+
+  	while fibarr[2] < n
+
+  		if fibarr[2].even?
+  			finalsum += fibarr[2]
+  		end
+
+  		fibarr[0] = fibarr[1]
+  		fibarr[1] = fibarr[2]
+
+      fibarr[2] = fibarr[0] + fibarr[1]
+
+  	end
+
+  	return finalsum
+  end
+end
